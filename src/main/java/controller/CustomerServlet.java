df8fba0b93c650ebb1e5ae145933a22f8a508ff4
QuynhNB <nbquynh911@gmail.com>
2019-09-26 17:20:31 +0700

    Show Customer List

--- src/main/java/controller/CustomerServlet.java
+++ src/main/java/controller/CustomerServlet.java
@@ -1,15 +1,51 @@
 package controller;
 
+import model.Customer;
+import service.CustomerService;
+import service.CustomerServiceImpl;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class CustomerServlet extends HttpServlet {
-
+    private CustomerService customerService = new CustomerServiceImpl();
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Hello");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null){
+            action = "";
+        }
+        switch (action){
+            case "create":
+                break;
+            case "edit":
+                break;
+            case "delete":
+                break;
+            case "view":
+                break;
+            default:
+                listCustomers(request, response);
+                break;
+        }
+    }
+
+    private void listCustomers(HttpServletRequest request, HttpServletResponse response) {
+        List<Customer> customers = this.customerService.findAll();
+        request.setAttribute("customers", customers);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("customer/list.jsp");
+        try {
+            dispatcher.forward(request, response);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
